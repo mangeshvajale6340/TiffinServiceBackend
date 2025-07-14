7c1489e4484238659c5ef2ff1947ed609da9165d
Mangesh Vajale <mangeshvajale1631@gmail.com>
2025-07-13 20:43:58 -0700

    Initial commit

---
+++ src/main/java/com/tiffinservice/model/User.java
@@ -0,0 +1,16 @@
+package com.tiffinservice.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String role; // CUSTOMER or ADMIN
+    private String address;
+    private String phone;
+}
